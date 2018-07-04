ada96222c4d4fdb106cd8cfc3160815e15e71506
Marvin Goesmann <mgo81@uclive.ac.nz>
2018-07-04 20:53:52 +1200

    added db constraints

--- backend/scripts/schema.sql
+++ backend/scripts/schema.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.21)
 # Database: quiz_server
-# Generation Time: 2018-07-04 05:21:19 +0000
+# Generation Time: 2018-07-04 08:52:48 +0000
 # ************************************************************
 
 
@@ -27,9 +27,11 @@ DROP TABLE IF EXISTS `answers`;
 
 CREATE TABLE `answers` (
   `answer_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `answer_question_id` int(11) NOT NULL,
+  `answer_question_id` int(11) unsigned NOT NULL,
   `answer_content` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`answer_id`)
+  PRIMARY KEY (`answer_id`),
+  KEY `answer_question_id` (`answer_question_id`),
+  CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`answer_question_id`) REFERENCES `questions` (`question_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -41,9 +43,11 @@ DROP TABLE IF EXISTS `courses`;
 
 CREATE TABLE `courses` (
   `course_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `course_teacher_id` int(11) NOT NULL,
+  `course_teacher_id` int(11) unsigned NOT NULL,
   `course_name` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`course_id`)
+  PRIMARY KEY (`course_id`),
+  KEY `course_teacher_id` (`course_teacher_id`),
+  CONSTRAINT `courses_ibfk_1` FOREIGN KEY (`course_teacher_id`) REFERENCES `teachers` (`teacher_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -55,9 +59,13 @@ DROP TABLE IF EXISTS `courses_students`;
 
 CREATE TABLE `courses_students` (
   `cs_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `cs_student_id` int(11) DEFAULT NULL,
-  `cs_courses_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`cs_id`)
+  `cs_student_id` int(11) unsigned NOT NULL,
+  `cs_course_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`cs_id`),
+  KEY `cs_student_id` (`cs_student_id`),
+  KEY `cs_course_id` (`cs_course_id`),
+  CONSTRAINT `courses_students_ibfk_1` FOREIGN KEY (`cs_student_id`) REFERENCES `students` (`student_id`),
+  CONSTRAINT `courses_students_ibfk_2` FOREIGN KEY (`cs_course_id`) REFERENCES `courses` (`course_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -69,25 +77,29 @@ DROP TABLE IF EXISTS `questions`;
 
 CREATE TABLE `questions` (
   `question_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `question_quiz_id` int(11) NOT NULL,
+  `question_quiz_id` int(11) unsigned NOT NULL,
   `question_description` varchar(500) NOT NULL DEFAULT '',
-  PRIMARY KEY (`question_id`)
+  PRIMARY KEY (`question_id`),
+  KEY `question_quiz_id` (`question_quiz_id`),
+  CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`question_quiz_id`) REFERENCES `quizzes` (`quiz_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
-# Dump of table quizes
+# Dump of table quizzes
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `quizes`;
+DROP TABLE IF EXISTS `quizzes`;
 
-CREATE TABLE `quizes` (
+CREATE TABLE `quizzes` (
   `quiz_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `quiz_course_id` int(11) NOT NULL,
+  `quiz_course_id` int(11) unsigned NOT NULL,
   `quiz_name` varchar(50) NOT NULL DEFAULT '',
-  `quiz_start_date` bigint(11) NOT NULL,
-  `quiz_end_date` bigint(11) NOT NULL,
-  PRIMARY KEY (`quiz_id`)
+  `quiz_start_date` bigint(11) unsigned NOT NULL,
+  `quiz_end_date` bigint(11) unsigned NOT NULL,
+  PRIMARY KEY (`quiz_id`),
+  KEY `quiz_course_id` (`quiz_course_id`),
+  CONSTRAINT `quizzes_ibfk_1` FOREIGN KEY (`quiz_course_id`) REFERENCES `courses` (`course_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -99,12 +111,14 @@ DROP TABLE IF EXISTS `students`;
 
 CREATE TABLE `students` (
   `student_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `student_teacher_id` int(11) NOT NULL,
+  `student_teacher_id` int(11) unsigned NOT NULL,
   `student_name` varchar(50) NOT NULL DEFAULT '',
   `student_email` varchar(128) NOT NULL DEFAULT '',
   `student_hash` varchar(128) NOT NULL DEFAULT '',
   `student_token` varchar(128) DEFAULT '',
-  PRIMARY KEY (`student_id`)
+  PRIMARY KEY (`student_id`),
+  KEY `student_teacher_id` (`student_teacher_id`),
+  CONSTRAINT `students_ibfk_1` FOREIGN KEY (`student_teacher_id`) REFERENCES `teachers` (`teacher_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -132,10 +146,12 @@ DROP TABLE IF EXISTS `tests`;
 
 CREATE TABLE `tests` (
   `test_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `test_question_id` int(11) DEFAULT NULL,
-  `test_input` varchar(256) DEFAULT NULL,
-  `test_expected` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`test_id`)
+  `test_question_id` int(11) unsigned NOT NULL,
+  `test_input` varchar(256) NOT NULL DEFAULT '',
+  `test_expected` varchar(256) NOT NULL DEFAULT '',
+  PRIMARY KEY (`test_id`),
+  KEY `test_question_id` (`test_question_id`),
+  CONSTRAINT `tests_ibfk_1` FOREIGN KEY (`test_question_id`) REFERENCES `questions` (`question_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
