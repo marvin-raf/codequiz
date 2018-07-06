cc20e79de3bbe2ce886e6234a3a53b31bc86235b
RafGDev <rafaelgoesmann@gmail.com>
2018-07-06 18:56:48 +1200

    Started frontend initialization

---
+++ frontend/src/containers/Sidebar/Sidebar.vue
@@ -0,0 +1,86 @@
+<template>
+    <v-navigation-drawer
+    id="sidebar"
+    dark
+    permanent
+  >
+    <v-list>
+      <v-list-tile
+        v-for="item in authItems"
+        :key="item.title"
+        @click=""
+      >
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+
+      </v-list-tile>
+        <v-divider></v-divider>
+
+      <v-list-tile
+        v-for="item in otherItems"
+        :key="item.title"
+        @click=""
+      >
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+
+      </v-list-tile>
+
+
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      authItems: [
+        {
+          title: "Signup",
+          url: "/signup",
+          icon: "lock"
+        },
+        {
+          title: "Signin",
+          url: "/signin",
+          icon: "lock"
+        }
+      ],
+      otherItems: [
+        {
+          title: "Quizzes",
+          url: "/quizzes",
+          icon: "pen"
+        },
+        {
+          title: "About",
+          url: "/about",
+          icon: ""
+        }
+      ]
+    };
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+@import "../../styles/variables.scss";
+#sidebar {
+  background-color: $wet-asphalt;
+  width: 200px !important;
+  height: 100%;
+}
+</style>
+
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,35 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./containers/App.vue";
+
+import VueRouter from "vue-router";
+import Vuetify from "vuetify";
+
+import Home from "./containers/Home/Home.vue";
+
+import authHelper from "./helpers/authHelper";
+
+import "vuetify/dist/vuetify.min.css"; // Ensure you are using css-loader
+
+Vue.use(Vuetify);
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    component: Home,
+    beforeEnter: authHelper.checkLoggedIn
+  }
+];
+
+const raf = async () => {};
+
+const router = new VueRouter({
+  routes,
+  mode: "history"
+});
 
 new Vue({
-  el: '#app',
+  el: "#app",
+  router,
   render: h => h(App)
-})
+});
