f4528ca731977089a5c3e7f3c9fddd8d45f6537f
Marvin Goesmann <mgo81@uclive.ac.nz>
2018-07-08 15:48:01 +1200

    Moved create quiz to courses

--- backend/app/packages/courses/controllers.py
+++ backend/app/packages/courses/controllers.py
@@ -41,3 +41,32 @@ def create():
     except Exception:
         return server_error()
     return created({"course_id": course_id})
+
+
+@courses_module.route("/<course_id>/quizzes", methods=["POST"])
+@teacher_signed_in
+def create_quiz(course_id):
+    """
+    Creates a quiz
+    """
+    try:
+        body = request.get_json()
+
+        teacher_id = request.teacher_id
+        name = body["name"]
+        start_date = body["start_date"]
+        end_date = body["end_date"]
+
+        if not models.course_exists(course_id,
+                                    teacher_id) or not models.check_dates(
+                                        start_date, end_date):
+            return bad_request()
+
+        quiz_id = models.insert_quiz(course_id, name, start_date, end_date)
+
+    except KeyError:
+        return bad_request()
+    except Exception as e:
+        print(e)
+        return server_error()
+    return created({"quiz_id": quiz_id})
--- backend/app/packages/courses/models.py
+++ backend/app/packages/courses/models.py
@@ -2,6 +2,7 @@
 Contains models for the Courses package
 """
 from app.util import db
+import time
 
 
 def get_courses(teacher_id):
@@ -31,3 +32,39 @@ def insert_course(teacher_id, name):
 
     course_id = db.insert_query(query, (teacher_id, name))
     return course_id
+
+
+def course_exists(course_id, teacher_id):
+    """
+    Checks if the course_id supplied exists
+    """
+    query = """
+    SELECT * FROM courses
+    WHERE course_id = %s
+    AND course_teacher_id = %s
+    """
+
+    course = db.query(query, (course_id, teacher_id))
+    return course
+
+
+def check_dates(start_date, end_date):
+    """
+    Returns true if dates are in the right order
+    """
+    current_date = int(time.time())
+    if current_date <= start_date and start_date <= end_date:
+        return True
+    return False
+
+
+def insert_quiz(course_id, name, start_date, end_date):
+    """
+    Inserts a quiz and returns its id
+    """
+    query = """
+    INSERT INTO quizzes (quiz_course_id, quiz_name, quiz_start_date, quiz_end_date)
+    VALUES (%s, %s, %s, %s)
+    """
+    quiz_id = db.insert_query(query, (course_id, name, start_date, end_date))
+    return quiz_id
